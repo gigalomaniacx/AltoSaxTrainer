745cbadd9a2568cabe95349388892b3d31c987e8
Adrian G <adgrjb@gmail.com>
2021-06-25 14:30:18 +0200

    Got the forloop printing options

--- js/scaleView.js
+++ js/scaleView.js
@@ -1,23 +1,32 @@
 function scaleView(){
-    document.getElementById('app').innerHTML = /*html*/ `
+    let dropDown ='';
+    for (let i = 0; i < model.notes.length; i++){
+    dropDown += dropDownOptions(model.notes[i]);
+    }
+
+    document.getElementById('app').innerHTML =  /*html*/ `
         <h1>scalePage</h1>
         <select onchange="noteImgChanger()" id="notes">
-            <option value="fullChart">Full chart</option>
-            <option value="A#-Bb">A# Bb</option>
-            <option value="B-Cb">B Cb</option>
-            <option value="B#-C">B# C</option>
-            <option value="C#-Db">C# Db</option>
-            <option value="D">D</option>
-        </select><br>
-        <img src="${model.noteImg}" width=${model.noteImgWidth} />
+            ${dropDown}
+        </select><br/>
+        <img src="${model.noteImg}" width="${model.noteImgWidth}" />
         <button onclick="mainPageView()">main page</button>
     `;
+
+    // <select onchange="noteImgChanger()" id="notes">
+    //         <option value="fullChart">Full chart</option>
+    //         <option value="A#-Bb">A# Bb</option>
+    //         <option value="B-Cb">B Cb</option>
+    //         <option value="B#-C">B# C</option>
+    //         <option value="C#-Db">C# Db</option>
+    //         <option value="D">D</option>
+    //     </select><br></br>
 }
 
 function noteImgChanger(){
     let imgchanger = document.getElementById('notes');
     imgchanger.value == 'fullChart' ? setImgAndWidth("./src/sax-fingering-chart-final.png",'"600vw"'):
-    imgchanger.value == 'A#-Bb' ? setImgAndWidth("./src/LowAsh-Bb-Finger.png", "60vw"):
+    imgchanger.value == 'A#-Bb' ? setImgAndWidth("./src/LowAsh-Bb-Finger.png", "140vw"):
     imgchanger.value == 'B#-C' ? setImgAndWidth("./src/LowBsh-C-Finger.png", "140vw"):
     imgchanger.value == 'C#-Db' ? setImgAndWidth("./src/LowB-Cb-Finger.png","140vw"):
     imgchanger.value == 'D' ? setImgAndWidth("./src/LowD-Finger.png","140vw"):
@@ -29,4 +38,8 @@ function noteImgChanger(){
 function setImgAndWidth(imgSrc,imgWidth){
     model.noteImg = imgSrc;
     model.noteImgWidth = imgWidth;
+}
+
+function dropDownOptions(value){
+return `<option value="${value}">${value}</option>`;
 }
