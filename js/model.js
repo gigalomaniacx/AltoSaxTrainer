bd14dffd1a844937d617c87ad2150b456259f240
Adrian G <adgrjb@gmail.com>
2021-06-25 14:31:10 +0200

    Got the start of a model going

--- js/model.js
+++ js/model.js
@@ -3,8 +3,8 @@ const model = {
      noteImg : "./src/sax-fingering-chart-final.png",
      noteImgWidth: "600vw",
      notes : [
-         'A#-Bb', 'B-Cb', 'B#-C', 'C#-Db', 'D'/*,'D#', 'Eb', 'E',
-         'Fb', 'E#', 'F', 'F#', 'Gb', 'G', 'G#', 'Ab', 'A'*/
+        'fullChart', 'A#-Bb', 'B-Cb', 'B#-C', 'C#-Db', 'D'/*,'D#', 'Eb', 'E',
+        'Fb', 'E#', 'F', 'F#', 'Gb', 'G', 'G#', 'Ab', 'A'*/
      ],
      
 }
