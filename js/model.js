aa1056cdec92f309ec23621a1e2011ce59d63c6c
Adrian G <adgrjb@gmail.com>
2021-06-28 13:23:58 +0200

    Added every note in the array & dropdown menu

--- js/model.js
+++ js/model.js
@@ -3,8 +3,10 @@ const model = {
      noteImg : "./src/sax-fingering-chart-final.png",
      noteImgWidth: "600vw",
      notes : [
-        'fullChart', 'A#-Bb', 'B-Cb', 'B#-C', 'C#-Db', 'D'/*,'D#', 'Eb', 'E',
-        'Fb', 'E#', 'F', 'F#', 'Gb', 'G', 'G#', 'Ab', 'A'*/
+        'fullChart', 'LowA#-Bb', 'LowB-Cb', 'LowB#-C', 'LowC#-Db', 'LowD','LowD#-Eb', 'LowE-Fb',
+        'LowE#-F', 'LowF#-Gb', 'LowG', 'LowG#-Ab', 'LowA', 'A#', 'Bb',
+        'B-Cb', 'B#', 'C', 'C#-Db', 'HighD', 'HighD#-Eb', 'HighE-Fb',
+        'HighE#-F', 'HighF#', 'HighGb', 'HighG', 'HighG#-Ab', 'HighA',
      ],
      
 }
--- js/scaleView.js
+++ js/scaleView.js
@@ -26,11 +26,34 @@ function scaleView(){
 function noteImgChanger(){
     let imgchanger = document.getElementById('notes');
     imgchanger.value == 'fullChart' ? setImgAndWidth("./src/sax-fingering-chart-final.png", "600vw"):
-    imgchanger.value == 'A#-Bb' ? setImgAndWidth("./src/LowAsh-Bb-Finger.png", "140vw"):
-    imgchanger.value == 'B-Cb' ? setImgAndWidth("./src/LowB-Cb-Finger.png", "140vw"):
-    imgchanger.value == 'B#-C' ? setImgAndWidth("./src/LowBsh-C-Finger.png", "140vw"):
-    imgchanger.value == 'C#-Db' ? setImgAndWidth("./src/LowCsh-Db-Finger.png", "140vw"):
-    imgchanger.value == 'D' ? setImgAndWidth("./src/LowD-Finger.png", "140vw"):
+    imgchanger.value == 'LowA#-Bb' ? setImgAndWidth("./src/LowAsh-Bb-Finger.png", "140vw"):
+    imgchanger.value == 'LowB-Cb' ? setImgAndWidth("./src/LowB-Cb-Finger.png", "140vw"):
+    imgchanger.value == 'LowB#-C' ? setImgAndWidth("./src/LowBsh-C-Finger.png", "140vw"):
+    imgchanger.value == 'LowC#-Db' ? setImgAndWidth("./src/LowCsh-Db-Finger.png", "140vw"):
+    imgchanger.value == 'LowD' ? setImgAndWidth("./src/LowD-Finger.png", "140vw"):
+    imgchanger.value == 'LowD#-Eb' ? setImgAndWidth("./src/LowDsh-Eb-Finger.png", "140vw"):
+    imgchanger.value == 'LowE-Fb' ? setImgAndWidth("./src/LowE-Fb-Finger.png", "140vw"):
+    imgchanger.value == 'LowE#-F' ? setImgAndWidth("./src/LowEsh-F-Finger.png", "140vw"):
+    imgchanger.value == 'LowF#' ? setImgAndWidth("./src/LowFsh-Finger.png", "140vw"):
+    imgchanger.value == 'LowGb' ? setImgAndWidth("./src/LowGb-Finger.png", "140vw"):
+    imgchanger.value == 'LowG' ? setImgAndWidth("./src/LowG-Finger.png", "140vw"):
+    imgchanger.value == 'LowG#-Ab' ? setImgAndWidth("./src/LowGsh-Ab-Finger.png", "140vw"):
+    imgchanger.value == 'LowA' ? setImgAndWidth("./src/LowA-Finger.png", "140vw"):
+    imgchanger.value == 'A#' ? setImgAndWidth("./src/Ash-Finger.png", "140vw"):
+    imgchanger.value == 'Bb' ? setImgAndWidth("./src/Bb-Finger.png", "140vw"):
+    imgchanger.value == 'B-Cb' ? setImgAndWidth("./src/B-Cb-Finger.png", "140vw"):
+    imgchanger.value == 'B#' ? setImgAndWidth("./src/Bsh-Finger.png", "140vw"):
+    imgchanger.value == 'C' ? setImgAndWidth("./src/C-Finger.png", "140vw"):
+    imgchanger.value == 'C#-Db' ? setImgAndWidth("./src/CshDb-Finger-Finger.png", "140vw"):
+    imgchanger.value == 'HighD' ? setImgAndWidth("./src/HighD-Finger.png", "140vw"):
+    imgchanger.value == 'HighD#-Eb' ? setImgAndWidth("./src/HighDsh-Eb-Finger.png", "140vw"):
+    imgchanger.value == 'HighE-Fb' ? setImgAndWidth("./src/HighE-Fb-Finger.png", "140vw"):
+    imgchanger.value == 'HighE#-F' ? setImgAndWidth("./src/HighEsh-F-Finger.png", "140vw"):
+    imgchanger.value == 'HighF#' ? setImgAndWidth("./src/HighFsh-Finger.png", "140vw"):
+    imgchanger.value == 'HighGb' ? setImgAndWidth("./src/HighGb-Finger.png", "140vw"):
+    imgchanger.value == 'HighG' ? setImgAndWidth("./src/HighG-Finger.png", "140vw"):
+    imgchanger.value == 'HighG#-Ab' ? setImgAndWidth("./src/HighGsh-Ab-Finger.png", "140vw"):
+    imgchanger.value == 'HighA' ? setImgAndWidth("./src/HighA-Finger.png", "140vw"):
     console.log('noteImgChanger not workie');
 
     scaleView();
