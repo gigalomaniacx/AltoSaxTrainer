5118ecd184188174ad2080f60aad44826793a825
Adrian <adgrjb@gmail.com>
2021-09-23 11:21:24 +0200

    extended Notes img array

--- js/scaleView.js
+++ js/scaleView.js
@@ -1,16 +1,29 @@
 // Draws the page
-function scaleView(){
-    let dropDown ='';
-    for (let i = 0; i < model.fingerchart.length; i++){
-    dropDown += dropDownOptions(model.fingerchart[i].name);
+function scaleView() {
+    let dropDown = '';
+    let noteDropDown = '';
+    for (let i = 0; i < model.fingerchart.length; i++) {
+        dropDown += dropDownOptions(model.fingerchart[i].name);
+    }
+    for (let i = 0; i < model.notes.length; i++) {
+        noteDropDown += dropDownOptions(model.notes[i].name);
     }
 
     document.getElementById('app').innerHTML =  /*html*/ `
         <h1>scalePage</h1>
+        <select onchange="notesImgChanger(this.value)" id="note">
+            ${noteDropDown}
+        </select>
+        <label for="notesChk">Notes</label>
+        <input type="checkbox" id="noteCheck" ${model.inputs.noteChk} onclick="checkCheck('noteCheck')"></label>
         <select onchange="noteImgChanger(this.value)" id="notes">
             ${dropDown}
-        </select><br/>
+        </select>
+        <input type="checkbox" id="fingerCheck" ${model.inputs.fingerChk} onclick="checkCheck('fingerCheck')">
+        <label for="fingerChk">Fingering</label>
+        <br/>
         <img src="${model.noteImg}" width="${model.noteImgWidth}" />
+        <img src="${model.notesImg}" width="${model.notesImgWidth}" /></br>
         <button onclick="mainPageView()">main page</button>
         <button onclick="timerView()">Timer</button>
     `;
@@ -18,6 +31,6 @@ function scaleView(){
 
 
 // Prints the options in the dropdown menu
-function dropDownOptions(value){
+function dropDownOptions(value) {
     return `<option ${getSelectedDropdown(value)} value="${value}">${value}</option>`;
-    }
+}
